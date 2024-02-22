d90b9022f1c1d216179386d184d1bde3f249969c
Nevohteeb <ciaranslow@outlook.com>
2024-02-23 08:31:37 +1300

    update products

--- app/(routes)/category/[categoryId]/page.tsx
+++ app/(routes)/category/[categoryId]/page.tsx
@@ -41,7 +41,7 @@ const CategoryPage: React.FC<CategoryPageProps> = async ({
                 <Billboard
                     data={category.billboard}
                 />
-                <div className="px-4 sm:px-6 lg:px-8">
+                <div className="px-4 sm:px-6 lg:px-8 pb-24">
                     <div className="lg:grid lg:grid-cols-5 lg:gap-x-8">
                         <MobileFilters sizes={sizes} colors={colors}/>
                         <div className="hidden lg:block">
